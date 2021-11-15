58edb756146f3e5078408e036b8ae67024123fb5
Mohak Sharma <mohak327@gmail.com>
2021-11-16 00:29:11 +0530

    exp 9

--- exp 9/Dijkstras.cpp
+++ exp 9/Dijkstras.cpp
@@ -1,75 +1,68 @@
-#include <iostream>
-#include <climits>
+#include<iostream>
+#include<vector>
+#include<set>
 using namespace std;
 
-// this method returns a minimum distance for the
-// vertex which is not included in Tset.
-int minimumDist(int dist[], bool Tset[])
-{
-    int min = INT_MAX, index;
-
-    for (int i = 0; i < 6; i++)
-    {
-        if (Tset[i] == false && dist[i] <= min)
-        {
-            min = dist[i];
-            index = i;
-        }
-    }
-    return index;
-}
-
-void Dijkstra(int **graph, int n, int src) // adjacency matrix used is 6x6
-{
-    // integer array to calculate minimum distance for each node.
-    int* dist = new int[n];
-    // boolean array to mark visted/unvisted for each node.
-    bool* Tset = new bool[n];
-
-    // set the nodes with infinity distance except for the initial node and mark them unvisited.
-    for (int i = 0; i < n; i++)
-    {
-        dist[i] = INT_MAX;
-        Tset[i] = false;
-    }
-
-    dist[src] = 0; // Source vertex distance is set to zero.
-
-    for (int i = 0; i < n; i++)
-    {
-        int m = minimumDist(dist, Tset); // vertex not yet included.
-        Tset[m] = true;                  // m with minimum distance included in Tset.
-        for (int i = 0; i < n; i++)
-        {
-            // Updating the minimum distance for the particular node.
-            if (!Tset[i] && graph[m][i] && dist[m] != INT_MAX && dist[m] + graph[m][i] < dist[i])
-                dist[i] = dist[m] + graph[m][i];
-        }
-    }
-    
-    cout << "Vertex\t\tDistance from source" << endl;
-    for (int i = 0; i < 6; i++)
-    {                      //Printing
-        char str = 65 + i; // Ascii values for pritning A,B,C..
-        cout << str << "\t\t\t" << dist[i] << endl;
-    }
-}
-
-int main()
-{
-    int n;
-    cin >> n;
-
-    int** graph = new int*[n];
-	for(int i=0; i<n; i++){
-		graph[i] = new int[n];
-		for(int j=0; j<n; j++){
-			cin >> graph[i][j];
-		}
+class Graph {
+public:
+	int v;
+	vector<pair<int,int>> *m;
+	Graph(int v){
+		this->v = v;
+		m = new vector<pair<int,int>>[v+1];
 	}
+	void addEdge(int s,int d,int wt){
+		m[s].push_back({d,wt});
+		m[d].push_back({s,wt});
+	}
+	void dijkstra(int src){
+		set<pair<int,int>> s;
+		vector<int> dist(this->v,2147483647);
+		dist[src] = 0;
+		s.insert({0,src}); // wt src
+		while(!s.empty()){
+			auto t = s.begin();
+			s.erase(s.begin());
+			int wt = t->first;
+			int node = t->second;
 
-    Dijkstra(graph, n, 0);
+			for(auto nbrs:m[node]){
+				int nbr = nbrs.first;
+				int nbr_wt = nbrs.second;
 
-    delete[] graph;
-    return 0;
+				if(wt+nbr_wt < dist[nbr]){
+					auto it = s.find({dist[nbr],nbr});
+					if(it!=s.end())
+						s.erase(it);
+
+					dist[nbr] = wt + nbr_wt;
+					s.insert({dist[nbr],nbr});
+				}
+			}
+		}
+		cout<<"--------------------\nSrc\t->\tDest\n--------------------\n";
+		for(int i=1;i<=v;i++){
+			cout<<src<<"\t->\t"<<dist[i]<<endl;
+		}
+		return ;
+	}
+};
+
+int main() {
+	cout << "\t  Dijkstra Algorithm\n\n";
+	int n,m;
+	cout<<"Enter number of nodes: ";
+	cin>>n;
+	cout<<"Enter number of edges: ";
+	cin>>m;
+	Graph g(n);
+	cout<<"Enter all edges (src dest wt)\n";
+	for(int i=0;i<m;i++){
+		int x,y,wt;
+		cin>>x>>y>>wt;
+		g.addEdge(x,y,wt);
+	}
+	g.dijkstra(1);
+
+	return 0;
 }
